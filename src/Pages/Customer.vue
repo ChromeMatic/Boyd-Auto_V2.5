5fc24bdd937081bfc49492af874ff947a491a55b
Armani Brown <61952310+ChromeMatic@users.noreply.github.com>
2022-06-27 18:35:41 -0500

    added temp data

--- src/Pages/Customer.vue
+++ src/Pages/Customer.vue
@@ -1,19 +1,19 @@
 <script setup lang="ts">
-   import { ThemeStore } from "../stores/Theme.js"
-   import searchFilter from "../components/searchFilter.vue"
+  import { ThemeStore } from "../stores/Theme.js"
+  import searchFilter from "../components/searchFilter.vue"
 
-   const darkMode = ThemeStore();
+  const darkMode = ThemeStore();
 
-   interface car{
+  interface car{
     name: string,
     price: string,
     img: string
   }
 
- let Cars:car[] = [
+  let Cars:car[] = [
    {
     name:'nissan gtr r35',
-    price:'$2.6 mil JMD',
+    price:'$6.2 mil JMD',
     img:'https://images.unsplash.com/photo-1619330490306-78b4e6461da4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80'
    },
    {
@@ -30,8 +30,29 @@
     name:'Toyota supra 2021 mrk5',
     price:'$10.6 mil JMD',
     img:'https://images.unsplash.com/photo-1625078923337-e8e14188077b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1111&q=80'
+   },
+   {
+    name:'Subaru WRX',
+    price:'$ 1.2 mil JMD',
+    img:'https://images.unsplash.com/photo-1636074641063-1c2152f1b31e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1025&q=80'
+   },
+   {
+    name:'Nissan 350z',
+    price:"$ 1.4 mil JMD",
+    img:'https://images.unsplash.com/photo-1608231488616-4b242241ddf6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+   },
+   {
+    name:'Nissan 400z',
+    price:'$4.6 mil JMD',
+    img:'https://hips.hearstapps.com/hmg-prod/images/2023-nissan-z-mmp-1-1629244720.jpg?crop=0.897xw:0.957xh;0.0481xw,0.0426xh&resize=1200:*'
+   },
+   {
+    name:'Honda Fit',
+    price:'$ 1.1 mil JMD',
+    img:'https://images.unsplash.com/photo-1609676671207-d021525a635d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1092&q=80'
    }
- ]
+  ]
+
 </script>
 
 <template>
@@ -58,7 +79,7 @@
           </h1>
 
           <img 
-            class="rounded-md"
+            class="rounded-md h-48"
             :src="car.img"
           />
 
